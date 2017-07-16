d6a129ce99055a207a4712852bb7c86eb8c19d56
avaxxx <avaxxx@gmail.com>
2017-07-16 21:57:30 +0200

    test from github client
    description

--- ClientApp/app/components/home/home.ts
+++ ClientApp/app/components/home/home.ts
@@ -1,2 +1,11 @@
+import {inject} from 'aurelia-framework';
+import {EventAggregator} from 'aurelia-event-aggregator';
+import { WebAPI } from "../../web-api";
+
+@inject(WebAPI, EventAggregator)
 export class Home {
+
+    constructor(private api: WebAPI, private ea: EventAggregator){
+        let list = api.getContactList().then(result => console.log(result));
+    }
 }
